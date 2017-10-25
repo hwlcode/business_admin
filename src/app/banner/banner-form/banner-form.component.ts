e5071f276da4e52dee7b5466fa2f48d6fff84c93
hwlcode <hwlcode@163.com>
2017-10-25 16:37:58 +0800

    完成banner管理

--- src/app/banner/banner-form/banner-form.component.ts
+++ src/app/banner/banner-form/banner-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {DropzoneConfigInterface} from 'ngx-dropzone-wrapper';
+import {Http, URLSearchParams} from '@angular/http';
 
 @Component({
     selector: 'app-banner-form',
@@ -8,32 +10,50 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
     styleUrls: ['./banner-form.component.css']
 })
 export class BannerFormComponent implements OnInit {
-    formModel: FormGroup
+    formModel: FormGroup;
+    config: DropzoneConfigInterface = {
+        url: '/api/upload',
+        maxFiles: 1,
+        acceptedFiles: 'image/*'
+    };
 
-    constructor(public router: Router) {
+    constructor(public router: Router, public http: Http) {
         const fb = new FormBuilder();
         this.formModel = fb.group({
             name: ['', Validators.required],
-            link: ['', Validators.required]
+            link: ['', Validators.required],
+            banner: ['']
         })
     }
 
     ngOnInit() {
     }
 
-    cannel() {
-        this.router.navigateByUrl('/banners')
-    }
-
     save() {
+        if (this.formModel.valid && this.formModel.value.banner !== null) {
+            const url = '/api/saveBanner';
+            const self = this;
+            const params = new URLSearchParams();
+            console.log(this.formModel.value.name);
+            params.append('name', this.formModel.value.name);
+            params.append('link', this.formModel.value.link);
+            params.append('banner', this.formModel.value.banner);
 
+            this.http.post(url, params)
+                .map(res => res.json())
+                .subscribe(function (data) {
+                    if (data.code === 0) {
+                        self.router.navigateByUrl('/banners');
+                    }
+                });
+        }
     }
 
-    onUploadError() {
-
+    onUploadError(event: any) {
+        console.log(event);
     }
 
-    onUploadSuccess() {
-
+    onUploadSuccess(event: any) {
+        this.formModel.value.banner = event[1].id;
     }
 }
--- src/app/banner/banner-list/banner-list.component.ts
+++ src/app/banner/banner-list/banner-list.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Http} from '@angular/http';
+import * as moment from 'moment';
+
+moment.locale('zh-cn');
 
 @Component({
     selector: 'app-banner-list',
@@ -7,19 +11,62 @@ import {Router} from '@angular/router';
     styleUrls: ['./banner-list.component.css']
 })
 export class BannerListComponent implements OnInit {
+    public banners;
+    page: Number = 1;
+    collectionSize: Number = 0;
+    pageSize: Number = 3;
 
-    constructor(public router: Router) {
+    constructor(public router: Router, public http: Http) {
+
     }
 
     ngOnInit() {
-    }
-
-    nav(url: string) {
-        this.router.navigateByUrl(url);
+        this.getBanners().subscribe(
+            res => {
+                if (res.code === 0) {
+                    const json = res.data;
+                    json.map(item => {
+                        item.createdAt = moment(new Date(item.createdAt)).format('YYYY-MM-DD hh:mm');
+                    })
+                    this.banners = json;
+                    this.collectionSize = res.total;
+                }
+            }
+        )
     }
 
     create() {
         this.router.navigateByUrl('/banner/0');
     }
 
+    getBanners(page: number = 1) {
+        return this.http.get('/api/banners?q=' + page).map(res => res.json());
+    }
+
+    pageChange(page: number) {
+        this.getBanners(page).subscribe(
+            res => {
+                if (res.code === 0) {
+                    const json = res.data;
+                    json.map(item => {
+                        item.createdAt = moment(new Date(item.createdAt)).format('YYYY-MM-DD hh:mm');
+                    })
+                    this.banners = json;
+                    this.collectionSize = res.total;
+                }
+            }
+        );
+    }
+
+    remove(banner) {
+        this.http.get('/api/banner/delete/' + banner._id)
+            .map(res => res.json())
+            .subscribe(
+                res => {
+                    if (res.code === 0) {
+                        this.banners = res.data;
+                    }
+                }
+            );
+    }
 }
